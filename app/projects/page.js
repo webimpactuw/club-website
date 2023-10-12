f7a5a7c6f590796c681ae12cc05a26b475c1ed1f
mathlord2 <erixrekt69@gmail.com>
2023-10-11 22:22:45 -0700

    Whoops, a bit more refactoring

--- app/projects/page.js
+++ app/projects/page.js
@@ -3,7 +3,6 @@ import Navbar from "../components/Navbar";
 export default function Projects() {
   return (
     <div>
-      <Navbar/>
       <h1>Projects</h1>
     </div>
   );
--- app/schedule/page.js
+++ app/schedule/page.js
@@ -3,7 +3,6 @@ import Navbar from "../components/Navbar";
 export default function Schedule() {
   return (
     <div>
-      <Navbar/>
       <h1>Schedule</h1>
     </div>
   );
