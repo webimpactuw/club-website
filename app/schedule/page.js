8f7cb93acea10a14e92c87f65f17024e457b7cfe
juliettepark <69448181+juliettepark@users.noreply.github.com>
2023-12-30 17:01:36 -0800

    Added Sanity config

--- app/schedule/page.js
+++ app/schedule/page.js
@@ -1,9 +1,21 @@
-import Navbar from "../components/Navbar";
-
 export default function Schedule() {
   return (
-    <div>
-      <h1>Schedule</h1>
+    <div className="m-14">
+      <div className="relative flex py-5 items-center">
+        <h1 className="font-bold text-4xl">Fall Workshop Schedule</h1>
+        <div className="mx-8 flex-grow border-t border-gray-400"></div>
+      </div>
+
+      <div className="m-14 font-bold text-xl">
+        <div className="flex ...">
+          <div className="flex-1 ...">Week</div>
+          <div className="contents">
+            <div className="flex-1 ...">Date</div>
+            <div className="flex-1 ...">Workshop</div>
+          </div>
+          <div className="flex-1 ...">Topic</div>
+        </div>
+      </div>
     </div>
   );
 }
