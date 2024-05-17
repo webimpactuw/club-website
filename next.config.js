22fe445de093cc047596736c5f902e8b0d3e44ea
blloop <42186044+blloop@users.noreply.github.com>
2024-05-16 20:53:41 -0700

    fix bad config and imports

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "cdn.geekwire.com", "apru.org"],
+    domains: ["cdn.sanity.io"],
   },
 };
 
