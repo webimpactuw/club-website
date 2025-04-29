b6ace29ab9cf7fac89d49a86ba5413c2ba1d6eca
blloop <42186044+blloop@users.noreply.github.com>
2025-04-29 12:57:33 -0700

    complete calendar design implementation

--- app/calendar/page.js
+++ app/calendar/page.js
@@ -48,9 +48,33 @@ import DateList from "../components/DateList";
 // }
 
 export default function About() {
+  const testEvents = [
+    {
+      day: 26,
+      title: "Coding",
+      events: [
+        "Beginner (5-6): Intro to web development",
+        "Advanced (6-7): Refresher on HTML/CSS/JS",
+      ],
+    },
+    {
+      day: 27,
+      title: "Club",
+      events: ["Kickoff Meeting (5-6): Learn how you can get involved!"],
+    },
+    {
+      day: 28,
+      title: "Design",
+      events: [
+        "Beginner (5-6): Intro to web design",
+        "Advanced (6-7): Refresher on visual hierarchy",
+      ],
+    },
+  ];
+
   return (
-    <div className="max-w-[96rem] mx-auto relative flex flex-col gap-6 md:gap-12 p-6 md:pt-12 md:px-20">
-      <DateList title="Fall Quarter 2024" projects={[]} show={true} />
+    <div className="max-w-[96rem] mx-auto relative flex flex-col gap-6 md:gap-12 p-6 md:py-12 md:px-20">
+      <DateList title="Fall Quarter 2024" events={testEvents} show={true} />
     </div>
   );
 }
--- app/components/DateList.js
+++ app/components/DateList.js
@@ -1,11 +1,17 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function DateList({ title, events, show }) {
   const [open, setOpen] = useState(show);
+  const [selected, setSelected] = useState(-1);
+  const [dayEvents, setDayEvents] = useState({});
 
-  const titles = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+  useEffect(() => {
+    setDayEvents(events.find((e) => e.day === selected));
+  }, [selected]);
+
+  const titles = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const datesPrevious = Array.from({ length: 5 }, (_, i) => i + 27);
   const datesCurrent = Array.from({ length: 30 }, (_, i) => i + 1);
 
@@ -39,21 +45,61 @@ export default function DateList({ title, events, show }) {
       </div>
 
       {open && (
-        <div className="space-y-4 h-auto mb-4 px-0 lg:px-12 gap-8 lg:gap-12 pb-12">
-          <p className="text-3xl font-black w-full">September</p>
-          <div className="flex flex-col md:flex-row gap-8 lg:gap-12 items-stretch">
-            <div className="grid grid-cols-7 gap-8 text-xl text-center">
-              {titles.map(e => 
+        <div className="h-auto mb-4 px-0 lg:px-12 gap-8 lg:gap-12">
+          <p className="text-3xl font-black w-full mb-6">September</p>
+          <div className="flex flex-col md:flex-row gap-12 items-stretch">
+            <div className="grid grid-cols-7 gap-x-8 gap-y-5 text-xl text-center">
+              {titles.map((e) => (
                 <p>{e}</p>
-              )}
-              {datesPrevious.map(e => 
-                <p className="opacity-25">{e}</p>
-              )}
-              {datesCurrent.map(e => 
-                <p>{e}</p>
+              ))}
+              {datesPrevious.map((e) => (
+                <p className="px-2 py-1.5 opacity-[30%]">{e}</p>
+              ))}
+              {datesCurrent.map((e) =>
+                events.some((event) => event.day === e) ? (
+                  selected === e ? (
+                    <button
+                      onClick={() => setSelected(-1)}
+                      className="px-2 py-1.5 relative bg-secondary text-grayLight rounded-full"
+                    >
+                      <p>{e}</p>
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => setSelected(e)}
+                      className="px-2 py-1.5 relative hover:bg-gray transition-colors rounded-full"
+                    >
+                      <p>{e}</p>
+                      <div className="absolute left-[45%] md:left-4 -bottom-0.5 w-2 h-2 bg-secondary rounded-full" />
+                    </button>
+                  )
+                ) : (
+                  <p className="px-2 py-1.5">{e}</p>
+                ),
               )}
             </div>
-            <div className="bg-secondary w-full md:w-60 shrink-0 rounded-3xl"></div>
+            {selected > -1 && dayEvents ? (
+              <div className="relative space-y-3 bg-secondary p-6 h-72 md:h-auto w-full md:w-[17rem] shrink-0 rounded-[2rem] text-white">
+                <div className="flex items-center gap-4">
+                  <p className="text-3xl font-bold">{dayEvents.title}</p>
+                  <hr className="flex-1 h-px bg-gray"></hr>
+                </div>
+                <p className="absolute left-4 -bottom-4 text-black opacity-25 font-bold text-[7rem]">
+                  {selected}
+                </p>
+                {dayEvents.events.map((e) => (
+                  <div>
+                    <p className="font-bold">
+                      {e.split(":")[0]}
+                      {":"}
+                    </p>
+                    <p className="font-light">{e.split(":")[1]}</p>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div className="bg-gray h-72 md:h-auto w-full md:w-[17rem] shrink-0 rounded-[2rem] text-white" />
+            )}
           </div>
         </div>
       )}
