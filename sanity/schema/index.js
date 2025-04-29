00463c572e31e5f3c57120d34a1be3b0619db546
blloop <42186044+blloop@users.noreply.github.com>
2025-04-29 11:41:55 -0700

    remove unused files

--- sanity/schema/index.js
+++
@@ -1,7 +0,0 @@
-import week from "./documents/week";
-import project from "./documents/project";
-import officer from "./documents/officer";
-
-export const schema = {
-  types: [week, project, officer],
-};
